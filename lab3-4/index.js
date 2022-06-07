eb07c8ef5140dee32481daa5b41c446badc00bd7
Elizaveta1412 <14122002lizard@gmail.com>
2022-06-07 04:59:11 +0300

    work on heroku deploy

---
+++ lab3-4/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home page</h1>')
+})
+
+app.get('/', (req, res) => {
+    res.end('<h1>About page</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log('Server has been started...')
+})
