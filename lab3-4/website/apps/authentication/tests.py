5618f134f2502ace30763daa054c0452985c98fa
Elizaveta1412 <14122002lizard@gmail.com>
2022-06-10 20:32:01 +0300

    add tests

--- lab3-4/website/apps/authentication/tests.py
+++ lab3-4/website/apps/authentication/tests.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+from .models import User
+
+
+class TestUser(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user('test_user', email=None, password='testpass')
+
+    def test_get_user(self):
+        user = User.objects.get(username='test_user')
+        self.assertTrue(user.check_password('testpass'))
+
+    def test_login(self):
+        login = self.client.login(username='test_user', password='testpass')
+        self.client.logout()
+
+    def test_logout(self):
+        response = self.client.get(f'/logout')
+        self.assertEqual(response.status_code, 200)
--- lab3-4/website/apps/home_page/tests.py
+++ lab3-4/website/apps/home_page/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def test_main_view(self):
+    response = self.client.get(f'')
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'base.html')
